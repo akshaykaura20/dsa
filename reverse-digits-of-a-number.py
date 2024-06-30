edf81c0e12504e0909a66b88c6875846fd9297cb
akshaykaura20 <akshaykaura0001@gmail.com>
2024-06-30 23:41:25 +0530

    added reverse-digits-of-a-number.py and few comment format changes

--- reverse-digits-of-a-number.py
+++ reverse-digits-of-a-number.py
@@ -1,21 +1,33 @@
-# NUMBER OF APPROACHES: TWO
-
-from math import log10
+# NUMBER OF APPROACHES: ONE
 
 # O(log_{10} N + 1) where N is the input number
 ############################################################################
 # Algorithm explanation
 
-# Count digits from behind by removing each digit by using integer division with 10,
-# until the number goes zero. This operator removes the trailing decimal part.
+# In order to reverse the number, we need digits from behind to come first in reversed number.
+# Hence, extract digits from behind by using modulus operator. But, how to construct the rev number?
+
+# Lets say we have the number = 1234. When we start reversing it from behind, we are basically doing:
+# 4 -> 43 (40 + 3) -> 432 (430 + 2) -> 4321 (4320 + 1)
+# Which is simply,
+# 4 -> 43 (4*10 + 3) -> 432 (43*10 + 2) -> 4321 (432*10 + 1)
+
+# We keep doing this, until the original number goes zero as we keep removing the extracted digit from
+# original number to again extract the next last_digit in next iteration.
+# This is done using the integer division operator.
 # For e.g. 1234 // 10 = 123 (int) i.e. the integer quotient
-def brute_force():
+def optimal_and_brute_force():
     n = int(input(""))
     num_of_digits = 0
+    reversed_number = 0
     while n != 0:
         num_of_digits += 1
+        last_digit = n % 10 # extract last digit
+        reversed_number = reversed_number * 10 + last_digit
+        # remove last digit
         n = n // 10 # integer division that leaves the part after decimal
-    print(count)
+    print(reversed_number)
+
 ############################################################################
 # Complexity explanation
 
@@ -35,21 +47,4 @@ def brute_force():
 # 
 # Hence, number of digits of a number can be approximated to log_{10}(N) + 1, which is the number of times,
 # the loop will run, and hence our time complexity is O(log_{10}(N) + 1)#
-############################################################################
-
-
-# O(1)
-############################################################################
-# Algorithm explanation
-# Simply calculate the expression int(log10(n)) + 1 = num of digits
-def optimal():
-    n = int(input(""))
-    num_of_digits = int(log10(n)) + 1
-    print(num_of_digits)
-############################################################################
-# Complexity explanation
-
-# As we have seen above in brute force approach, 
-# k = log_{10}(N) + 1 = num of digits in N
-# Hence, we simply calclulate the value of k using log10() method from math module.
 ############################################################################
