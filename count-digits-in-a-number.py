5f1318ad6debe0d2cc506833a9c83350b2c43944
akshaykaura20 <akshaykaura0001@gmail.com>
2024-06-30 22:57:25 +0530

    added optimal approach for count-digits-in-a-number.py

--- count-digits-in-a-number.py
+++ count-digits-in-a-number.py
@@ -1,12 +1,13 @@
+from math import log10
+
 # O(log_{10} N + 1) where N is number of
-n = int(input(""))
-count = 0
-
-while n != 0:
-    count += 1
-    n = n // 10 # integer division that leaves the part after decimal
-
-print(count)
+def brute_force():
+    n = int(input(""))
+    num_of_digits = 0
+    while n != 0:
+        num_of_digits += 1
+        n = n // 10 # integer division that leaves the part after decimal
+    print(count)
 ############################################################################
 # Complexity explanation
 
@@ -26,4 +27,18 @@ print(count)
 # 
 # Hence, number of digits of a number can be approximated to log_{10}(N) + 1, which is the number of times,
 # the loop will run, and hence our time complexity is O(log_{10}(N) + 1)#
+############################################################################
+
+
+# O(1)
+def optimal():
+    n = int(input(""))
+    num_of_digits = int(log10(n)) + 1
+    print(num_of_digits)
+############################################################################
+# Complexity explanation
+
+# As we have seen above in brute force approach, 
+# k = log_{10}(N) + 1 = num of digits in N
+# Hence, we simply calclulate the value of k using log10() method from math module.
 ############################################################################
